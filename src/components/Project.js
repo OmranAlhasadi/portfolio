b4b9521c988374be64ae4ef2cbba284a218c6428
Omran <omranelhasadi@gmail.com>
2023-09-26 16:24:08 +0800

    Trying to get Projects section right, not done

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,13 +4,15 @@ import ProjectInfo from "./ProjectInfo";
 const Project = ({ img, title, children }) => {
   return (
     <div className={styles.container}>
-      <div className={styles.shadow}></div>
-      <div
-        className={styles.image}
-        style={{ backgroundImage: `url(${img})` }}
-      ></div>
-      <div className={styles.info}>
-        <ProjectInfo title={title}>{children}</ProjectInfo>
+      <div className={styles.innerWrapper}>
+        <div className={styles.shadow}></div>
+        <div
+          className={styles.image}
+          style={{ backgroundImage: `url(${img})` }}
+        ></div>
+        <div className={styles.info}>
+          <ProjectInfo title={title}>{children}</ProjectInfo>
+        </div>
       </div>
     </div>
   );
