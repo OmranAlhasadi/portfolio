714ed217c1507c37962823ffc3db9519d2b7e1f5
Omran <omranelhasadi@gmail.com>
2024-06-07 20:08:51 +0800

    Added Bubbles project and removed older ones

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -4,13 +4,34 @@ import Project from "./Project";
 import easyGains from "../images/easyGains2.jpeg";
 import etch from "../images/etch2.png";
 import rps from "../images/RPS.png";
+import bubbles from "../images/Bubbles.png";
 import Divider from "./Divider";
 import Section from "./Section";
 
 const Projects = () => {
   return (
     <div className={styles.container}>
-      <Project img={easyGains} title="EasyGains" noButtons={true}>
+      <Project
+        img={bubbles}
+        title="Bubbles"
+        /* alternate={true} */
+        link1={"https://bubbles-mern-15bcc2144c14.herokuapp.com/login"}
+        link2={"https://github.com/OmranAlhasadi/bubbles"}
+      >
+        <p>
+          A social media app where you can create posts, upload images, add
+          friends...etc.
+        </p>
+        <p>This is a full stack project to showcase my full stack knowledge</p>
+        <Divider />
+        <i>Built using MERN stack (Mongo, Express, React and Node) </i>
+      </Project>
+      <Project
+        img={easyGains}
+        title="EasyGains"
+        alternate={true}
+        noButtons={true}
+      >
         <p>
           EasyGains is a macronutrient and calories logger that is specifically
           catered for Bodybuilders. It calculates how many calories and macros
@@ -23,38 +44,6 @@ const Projects = () => {
         </p>
         <Divider />
         <i>Built using Python, Dart and firebase</i>
-      </Project>
-      <Project
-        img={etch}
-        title="Etch-a-Sketch"
-        alternate={true}
-        link1={"https://omranalhasadi.github.io/etch-a-sketch/"}
-        link2={"https://github.com/OmranAlhasadi/etch-a-sketch"}
-      >
-        <p>
-          A fun etch-a-sketch made using vanilla js when I was learning. you can
-          color, erase, reset, choose the grid dimensions and has a random
-          rainbow mode.{" "}
-        </p>
-        <p>
-          Please use google chrome when viewing it since I used chrome webkits
-          in styling it which will break in other browsers
-        </p>
-        <Divider />
-        <i>Built using HTML, CSS, Javascript </i>
-      </Project>
-      <Project
-        img={rps}
-        title="RPS"
-        link1={"https://omranalhasadi.github.io/rock-paper-scissors/"}
-        link2={"https://github.com/OmranAlhasadi/rock-paper-scissors"}
-      >
-        <p>
-          A Rock-Paper-Scissors game I made while learning vanilla JS. it is a
-          first to 5 wins against a random choice computer opponent
-        </p>
-        <Divider />
-        <i>Built using HTML, CSS, Javascript </i>
       </Project>
     </div>
   );
